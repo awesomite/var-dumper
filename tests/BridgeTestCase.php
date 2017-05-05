7159ce35e56c3afd781fb80e819eafe57a74b73c
bkrukowski <krukowski.bartlomiej@gmail.com>
2017-05-05 18:20:10 +0200

    Removed class_alias, because IDE cannot suggest functions

--- tests/BridgeTestCase.php
+++ tests/BridgeTestCase.php
@@ -1,7 +1,21 @@
 <?php
 
+namespace Awesomite\VarDumper;
+
+use PHPUnit\Framework\TestCase;
+
 if (class_exists('PHPUnit_Framework_TestCase')) {
-    class_alias('PHPUnit_Framework_TestCase', 'Awesomite\VarDumper\BridgeTestCase');
-} else {
-    class_alias('PHPUnit\Framework\TestCase', 'Awesomite\VarDumper\BridgeTestCase');
+    /**
+     * @internal
+     */
+    abstract class BridgeTestCase extends \PHPUnit_Framework_TestCase
+    {
+    }
+} elseif (class_exists('PHPUnit\Framework\TestCase')) {
+    /**
+     * @internal
+     */
+    abstract class BridgeTestCase extends TestCase
+    {
+    }
 }

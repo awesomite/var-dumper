3a0f2302a7de0b98f3f41a00e0e5e5912d3885ba
bkrukowski <krukowski.bartlomiej@gmail.com>
2017-08-29 23:56:43 +0200

    Added example of __debugInfo() usage

---
+++ examples/debug-info.php
@@ -0,0 +1,36 @@
+<?php
+
+use Awesomite\VarDumper\LightVarDumper;
+
+require __DIR__ . DIRECTORY_SEPARATOR . 'init.php';
+
+/**
+ * @internal
+ */
+class MyClass
+{
+    public function __debugInfo()
+    {
+        return array(
+            'key'=> 'value',
+            'some' => 'output',
+        );
+    }
+}
+
+$dumper = new LightVarDumper();
+$dumper->dump(new MyClass());
+
+/*
+
+Output:
+
+object(MyClass) #1 (1) {
+    public $__debugInfo() =>
+        array(2) {
+            [key] =>  «value»
+            [some] => «output»
+        }
+}
+
+*/

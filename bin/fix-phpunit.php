536f94509b0a80a7f2cbc907d48b9988770bd6e2
bkrukowski <krukowski.bartlomiej@gmail.com>
2017-12-05 00:14:00 +0100

    Fix for old phpunit, `each` function is deprecated in php 7.2

--- bin/fix-phpunit.php
+++ bin/fix-phpunit.php
@@ -7,15 +7,5 @@ $reflection = new ReflectionClass('PHPUnit_Util_Getopt');
 $file = $reflection->getFileName();
 $contents = \file_get_contents($file);
 
-$search = <<<'CODE'
-        while (list($i, $arg) = each($args)) {
-CODE;
-
-$replace = <<<'CODE'
-        while (false !== $arg = current($args)) {
-            $i = key($args);
-            next($args);
-CODE;
-
-$contents = \str_replace($search, $replace, $contents);
+$contents = \str_replace('= each(', '= awesomite_each(', $contents);
 \file_put_contents($file, $contents);
---
+++ tests/functions.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @internal
+ * @param array $array
+ * @return array|false
+ */
+function awesomite_each(array &$array)
+{
+    if (false !== $arg = \current($array)) {
+        $i = \key($array);
+        \next($array);
+
+        return array(
+            1 => $arg,
+            'value' => $arg,
+            0 => $i,
+            'key' => $i,
+        );
+    }
+
+    return false;
+}

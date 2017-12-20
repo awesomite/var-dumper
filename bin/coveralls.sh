f39d75d232685962afdb61b8d6797fc52cc26c74
bkrukowski <krukowski.bartlomiej@gmail.com>
2017-12-20 21:02:32 +0100

    * use php-coveralls/php-coveralls instead of satooshi/php-coveralls * support for php-coveralls/php-coveralls ^1.0.1 || ^2.0.0

--- bin/coveralls.sh
+++ bin/coveralls.sh
@@ -3,7 +3,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if php -r "var_export(get_loaded_extensions(true));" | grep --quiet -i xdebug; then
-  php ${DIR}/../vendor/bin/coveralls -v
+  if [ -f ${DIR}/../vendor/bin/coveralls ]; then
+    php ${DIR}/../vendor/bin/coveralls -v
+  else
+    php ${DIR}/../vendor/bin/php-coveralls -v
+  fi
 else
   echo "Xdebug is not installed"
 fi

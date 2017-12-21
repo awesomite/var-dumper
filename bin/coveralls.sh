f1033572d39adca5debadd48e93c660ae1545060
bkrukowski <krukowski.bartlomiej@gmail.com>
2017-12-21 22:30:41 +0100

    Moved php-coveralls and php-cs-fixer to separated composer.json

--- bin/coveralls.sh
+++ bin/coveralls.sh
@@ -2,17 +2,11 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [[ "$DEPENDENCIES" = 'low' ]]; then
-  export PHP_ARGS=$(php -r "echo '-d error_reporting=', E_ALL ^ (E_DEPRECATED | E_USER_DEPRECATED);");
-else
-  export PHP_ARGS='';
-fi
-
 if php -r "var_export(get_loaded_extensions(true));" | grep --quiet -i xdebug; then
-  if [ -f ${DIR}/../vendor/bin/coveralls ]; then
-    php ${PHP_ARGS} ${DIR}/../vendor/bin/coveralls -v
+  if [ -f ${DIR}/../dev-tools/vendor/bin/coveralls ]; then
+    php ${PHP_ARGS} ${DIR}/../dev-tools/vendor/bin/coveralls -v
   else
-    php ${PHP_ARGS} ${DIR}/../vendor/bin/php-coveralls -v
+    php ${PHP_ARGS} ${DIR}/../dev-tools/vendor/bin/php-coveralls -v
   fi
 else
   echo "Xdebug is not installed"

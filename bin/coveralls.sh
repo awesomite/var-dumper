ebc9e0a084778c503a873a31859f5986afb7b9b0
bkrukowski <krukowski.bartlomiej@gmail.com>
2017-12-21 21:59:50 +0100

    https://travis-ci.org/awesomite/var-dumper/jobs/319902195
    PHP 5.3.29
    PHPUnit 4.8.36 by Sebastian Bergmann and contributors.
    ....................PHP Fatal error:  Cannot access private property Awesomite\VarDumper\Subdumpers\StringDumper::$whiteChars in /home/travis/build/awesomite/var-dumper/src/Subdumpers/StringDumper.php on line 113

--- bin/coveralls.sh
+++ bin/coveralls.sh
@@ -2,11 +2,17 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+if [[ "$DEPENDENCIES" = 'low' ]]; then
+  export PHP_ARGS=$(php -r "echo '-d error_reporting=', E_ALL ^ (E_DEPRECATED | E_USER_DEPRECATED);");
+else
+  export PHP_ARGS='';
+fi
+
 if php -r "var_export(get_loaded_extensions(true));" | grep --quiet -i xdebug; then
   if [ -f ${DIR}/../vendor/bin/coveralls ]; then
-    php ${DIR}/../vendor/bin/coveralls -v
+    php ${PHP_ARGS} ${DIR}/../vendor/bin/coveralls -v
   else
-    php ${DIR}/../vendor/bin/php-coveralls -v
+    php ${PHP_ARGS} ${DIR}/../vendor/bin/php-coveralls -v
   fi
 else
   echo "Xdebug is not installed"

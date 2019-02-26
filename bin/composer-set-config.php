b9afc145fc58c4f8d92021214c054833b696230c
bkrukowski <krukowski.bartlomiej@gmail.com>
2019-02-26 23:44:08 +0400

    Execute HHVM tests in travis

--- bin/composer-set-config.php
+++ bin/composer-set-config.php
@@ -1,13 +1,28 @@
 #!/usr/bin/env php
 <?php
 
-$handle = function ($scriptName, $key, $value) {
+/**
+ * @param $scriptName
+ * @param $value
+ * @param ...$keys
+ */
+$handle = function ($scriptName, $value, $_) {
     $jsonPath = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'composer.json';
     $json = \json_decode(\file_get_contents($jsonPath), true);
     if (!isset($json['config'])) {
         $json['config'] = array();
     }
-    $json['config'][$key] = $value;
+
+    $config = &$json['config'];
+
+    foreach (array_slice(func_get_args(), 2, -1) as $currentKey) {
+        if (!isset($config[$currentKey])) {
+            $config[$currentKey] = [];
+        }
+        $config = &$config[$currentKey];
+    }
+
+    $config[array_slice(func_get_args(), -1, 1)[0]] = $value;
 
     $options = JSON_UNESCAPED_UNICODE;
     if (\defined('JSON_PRETTY_PRINT')) {

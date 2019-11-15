46b21a74364cfbb3c7dfd1c82f67df73273db41b
bkrukowski <krukowski.bartlomiej@gmail.com>
2019-11-16 03:21:55 +0400

    Test for HHVM

--- test.php
+++ test.php
@@ -2,9 +2,9 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-set_error_handler(function () {
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
     echo "\n\n";
-    var_dump(func_get_args());
+    var_dump($errfile, $errline);
     debug_print_backtrace();
     echo "\n\n";
 });

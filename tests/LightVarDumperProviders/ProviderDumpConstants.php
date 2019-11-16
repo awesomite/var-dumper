ff7afdeb5ff403e55ac4348dfe4d74e7ff61563f
bkrukowski <krukowski.bartlomiej@gmail.com>
2019-11-16 04:40:16 +0400

    php-cs-fixer

--- tests/LightVarDumperProviders/ProviderDumpConstants.php
+++ tests/LightVarDumperProviders/ProviderDumpConstants.php
@@ -25,14 +25,14 @@ final class ProviderDumpConstants implements \IteratorAggregate
         $result['INF'] = array(\INF, "INF\n");
         $result['NAN'] = array(\NAN, "NAN\n");
         if (\defined('PHP_INT_MIN')) {
-            $result['PHP_INT_MIN'] = array(PHP_INT_MIN, "PHP_INT_MIN\n");
+            $result['PHP_INT_MIN'] = array(\PHP_INT_MIN, "PHP_INT_MIN\n");
         } else {
             $result['PHP_INT_MIN'] = array(~\PHP_INT_MAX, "PHP_INT_MIN\n");
         }
         $result['PHP_INT_MAX'] = array(\PHP_INT_MAX, "PHP_INT_MAX\n");
 
         if (\defined('PHP_FLOAT_EPSILON')) {
-            $result['PHP_FLOAT_EPSILON'] = array(PHP_FLOAT_EPSILON, "PHP_FLOAT_EPSILON\n");
+            $result['PHP_FLOAT_EPSILON'] = array(\PHP_FLOAT_EPSILON, "PHP_FLOAT_EPSILON\n");
         }
 
         return new \ArrayIterator($result);
